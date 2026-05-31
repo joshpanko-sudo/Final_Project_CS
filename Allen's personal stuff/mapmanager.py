9a819d367976b413cde12b3b47ad89dc9f6db3a7
StairForceOne <kerbalspaceprogramplayer1@gmail.com>
2026-05-31 16:59:03 -0600

    Hello There, testing

--- Allen's personal stuff/mapmanager.py
+++ Allen's personal stuff/mapmanager.py
@@ -1,2 +1,22 @@
+'''
+Legend:
+Name: MapManager
+Red highlight = error
+Green highlight = working
+Blue highlight = action
+Error guide: Warning: MapManager could not {Error Here}
+
+Yes, you could combine some aspects of this code, 
+but I want to favor reliablilty and redundancy
+
+
+'''
+
+try:
+    from tabulate import tabulate
+except ImportError as e:
+    print(f"Warning: MapManager could not load tabulate. {e}. Perhaps you have not installed it?")
+    quit()
+
 class mapManager():
     pass
