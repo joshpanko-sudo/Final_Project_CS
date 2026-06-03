21ce30f0a9fc0d48bfee2ae249c38d46ba4aa063
StairForceOne <kerbalspaceprogramplayer1@gmail.com>
2026-06-03 15:08:29 -0600

    changes

---
+++ Allen's personal stuff/map_managerV2.py
@@ -0,0 +1,14 @@
+'''
+Legend:
+- Name: MapManager
+
+Highlight Codes:
+- Red: Error
+- Green: Working
+- Blue: Action being done
+
+Error template:
+- Warning: MapManager could not {Error Here}. {e}. Please {Possible Solution Here}
+
+
+'''
