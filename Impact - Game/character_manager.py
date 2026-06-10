294368d5389335a843fc6a0c01c8e21f9770a31a
StairForceOne <kerbalspaceprogramplayer1@gmail.com>
2026-06-10 12:55:18 -0600

    Cannot stop making changes

--- Impact - Game/character_manager.py
+++ Impact - Game/character_manager.py
@@ -27,25 +27,30 @@ except ImportError as e:
 
 
 class CharacterManager():
-    def __init__(self, character_name:str, character_health:float) -> None:
+    def __init__(self, character_name:str, character_health:float, max_character_health:float = 100) -> None:
         self.name = character_name
         self.health = character_health
+        self.max_health = max_character_health
+        self.temporary_max_health = 0
         self.alive = True
         self.character_description = ""
+
 
     def __str__(self):
         return f"Character: {self.name}"
     
+
     def description(self, character_description:str, print_description:bool = False):
         self.character_description = character_description or "No description yet"
         if print_description:
             print(self.character_description)
         return None
     
+
     def character_status(self):
         status_data = [
             ["Name", self.name],
-            ["Health", f"{self.health:,}"],
+            ["Health", f"{self.health + self.temporary_max_health:,}"],
             ["Status", "Alive" if self.alive else "Dead"],
             ["Description", self.character_description]
         ]
@@ -53,7 +58,15 @@ class CharacterManager():
 
 
     def take_damage(self, amount_of_damage:float, damaged_by:str = "Unknown"):
-        self.health -= amount_of_damage
+        if self.temporary_max_health > 0:
+            if amount_of_damage <= self.temporary_max_health:
+                self.temporary_max_health -= amount_of_damage
+                amount_of_damage = 0
+            else:
+                amount_of_damage -= self.temporary_max_health
+                self.temporary_max_health = 0
+        if amount_of_damage > 0:
+            self.health -= amount_of_damage
         if self.health <= 0:
             self.health = 0
             self.alive = False
@@ -63,9 +76,22 @@ class CharacterManager():
             print(f"{self.name} took damaged. Current health: {self.health}") if damaged_by == "Unknown" else print(f"{self.name} took damage from {damaged_by}. Current health: {self.health}")
             time.sleep(1) 
 
+
     def heal(self, healing_amount:float, healing_item:str = "Unknown"):
+        # Add code in the case if an item is a revive item, the character can come back to life.
         if not self.alive:
             print(f"{self.name} is no longer alive and cannot be healed.")
+            return
+        totel_health = self.health + healing_amount
+        if totel_health > self.max_health:
+            overflow = total_health - self.max_health
+            self.temporary_max_health += overflow
+            self.health = self.max_health
+        else:
+            self.health = total_health
+
+
+        
 
 
 
