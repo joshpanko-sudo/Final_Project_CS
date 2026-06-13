09acd1ec3e372856b25430e292c54ad3686ada0c
StairForceOne <kerbalspaceprogramplayer1@gmail.com>
2026-06-13 11:16:31 -0600

    push

--- Impact - Game/character_managerV2.py
+++ Impact - Game/character_managerV2.py
@@ -41,3 +41,22 @@ try:
 except ImportError as e:
     print(f"Warning: CharacterManager could not load character_attacks_database.py. {e}. Perhaps you have not installed it?")
     quit()
+
+class CharacterManager:
+    # Defaults the max character health to 100
+    def __init__(self, character_name: str, character_health: float, max_character_health: float = 100) -> None:
+        self.name = character_name # The name of the character
+        self.health = character_health # Current character health (Does not have to be max)
+        self.max_health = max_character_health # Value cannot change, maximum health a character can have
+        self.temporary_max_health = 0 # Allows the character health to go above the max
+        self.alive = True # Character is alive and not defeated
+        self.character_description = "" # Info about character
+
+    def __str__(self):
+        return f"Character: {self.name}"
+    
+    def description(self, character_description: str, print_description: bool = False):
+        self.character_description = character_description or "No description yet"
+        if print_description:
+            print(self.character_description)
+        return None
